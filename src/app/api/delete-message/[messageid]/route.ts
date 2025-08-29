2e3410244c8fe75c9dc3cf49fa9f672556210d14
yhimanshu22 <himu09854@gmail.com>
2025-08-30 00:54:44 +0530

    fix error

--- src/app/api/delete-message/[messageid]/route.ts
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -3,7 +3,7 @@ import { getServerSession } from 'next-auth/next';
 import dbConnect from '@/lib/dbConnect';
 import { User } from 'next-auth';
 import { Message } from '@/model/User';
-import { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 import { authOptions } from '../../auth/[...nextauth]/options';
 
 export async function DELETE(
@@ -15,7 +15,7 @@ export async function DELETE(
   const session = await getServerSession(authOptions);
   const _user: User = session?.user;
   if (!session || !_user) {
-    return Response.json(
+    return NextResponse.json(
       { success: false, message: 'Not authenticated' },
       { status: 401 }
     );
@@ -28,19 +28,19 @@ export async function DELETE(
     );
 
     if (updateResult.modifiedCount === 0) {
-      return Response.json(
+      return NextResponse.json(
         { message: 'Message not found or already deleted', success: false },
         { status: 404 }
       );
     }
 
-    return Response.json(
+    return NextResponse.json(
       { message: 'Message deleted', success: true },
       { status: 200 }
     );
   } catch (error) {
     console.error('Error deleting message:', error);
-    return Response.json(
+    return NextResponse.json(
       { message: 'Error deleting message', success: false },
       { status: 500 }
     );
